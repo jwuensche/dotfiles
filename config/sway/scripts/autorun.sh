0c0e775a8f8388571ebb4755b7434fd0f88a2505
Johannes Wünsche <johannes@spacesnek.rocks>
2021-08-18 16:36:29 +0200

    Update waybar

--- config/sway/scripts/autorun.sh
+++ config/sway/scripts/autorun.sh
@@ -4,4 +4,5 @@ mako&
 lxpolkit&
 kanshi&
 /usr/libexec/xdg-desktop-portal-wlr&
-enkei /home/fred/Pictures/Lion/Mac\ OS\ X\ Lion.xml&
+waybar -c /home/fred/.config/waybar/config-dock -s /home/fred/.config/waybar/dock/style-dock.css
+enkei /home/fred/Pictures/WallpapersAll/Tohru_Kanna_Kobayashi_Excited_for_Compilers.png
