584cce648e95a50b98b53a8911cb3fa6d428f655
Johannes Wünsche <johannes@spacesnek.rocks>
2021-10-09 20:54:48 +0200

    update

--- config/sway/scripts/autorun.sh
+++ config/sway/scripts/autorun.sh
@@ -4,5 +4,6 @@ mako&
 lxpolkit&
 kanshi&
 /usr/libexec/xdg-desktop-portal-wlr&
-waybar -c /home/fred/.config/waybar/config-dock -s /home/fred/.config/waybar/dock/style-dock.css
-enkei /home/fred/Pictures/WallpapersAll/Tohru_Kanna_Kobayashi_Excited_for_Compilers.png
+waybar -c /home/fred/.config/waybar/dock/config-dock -s /home/fred/.config/waybar/dock/style-dock.css&
+dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus&
+enkei /home/fred/Pictures/Space/jpegPIA24810.jpg
