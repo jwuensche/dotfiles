bfdb1322d9bfd9de91bddb169c6ab9c34ecb92ff
Johannes Wünsche <johannes@spacesnek.rocks>
2022-02-17 18:43:07 +0100

    Housekeeping

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 rm config -r
 mkdir config
-cp ${HOME}/.config/{alacritty,waybar,sway,wofi,mako,wlogout,systemd,environment.d,doom,kanshi,wayfire.ini} config -r
+cp ${HOME}/.config/{alacritty,waybar,sway,wofi,mako,wlogout,systemd,environment.d,doom,kanshi,wayfire.ini,nvim} config -r
